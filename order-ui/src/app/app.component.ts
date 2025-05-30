1daf1094e05421a8c893cf5b1f08e9a8870b6892
Siim Sams <it.siim.sams@gmail.com>
2025-05-30 14:36:52 +0300

    Inti routes and components

--- order-ui/src/app/app.component.ts
+++ order-ui/src/app/app.component.ts
@@ -5,9 +5,12 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+  `
 })
 export class AppComponent {
   title = 'order-ui';
-}
+} 
--- order-ui/src/app/app.routes.ts
+++ order-ui/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { OrderListComponent } from './pages/order-list/order-list.component';
+import { OrderCreateComponent } from './pages/order-create/order-create.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'orders', component: OrderListComponent },
+  { path: 'orders/new', component: OrderCreateComponent },
+  { path: '', redirectTo: '/orders', pathMatch: 'full' }
+]; 
