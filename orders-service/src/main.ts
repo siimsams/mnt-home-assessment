2ff7fa32fa5bd86c4a6c24d66b40a94f6be25b4b
Siim Sams <it.siim.sams@gmail.com>
2025-06-01 13:43:28 +0300

    Set cors origin through param

--- orders-service/src/main.ts
+++ orders-service/src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { runMigrations } from './typeorm.config';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function runApplication() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:4200',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Accept'],
   });
